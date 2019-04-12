05af74354718f2bf1347e97710882e45af958fc2
Gregory Mundy <gregmundy@gmail.com>
2019-04-11 23:38:16 -0400

    fix: authlib now respect additional keyword arguments

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='brighthive-authlib',
-    version='1.0.2',
+    version='1.0.3',
     author='Gregory Mundy',
     author_email='greg@brighthive.io',
     description='BrightHive API Authorization and Authentication Library',
